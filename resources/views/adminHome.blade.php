cceba316108f3a4aacb25ccfcf5cd7fc0c5391df
vagrant <vagrant@homestead>
2018-07-27 06:23:43 +0000

    Admin home files

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -10,22 +10,18 @@
     <li><a href="#">
         <img src="images/nas.ico">
     <h2>NAS</h2>
-    <p>Nas Agencies - ACC</p></a>
     </li>
     <li><a href="#">
-        <img src="images/attach.png">
+        <img src="images/upn.png">
     <h2>UPN</h2>
-    <p>ACC marketing department</p></a>
     </li>
     <li><a href="#">
         <img src="images/smile.png">
     <h2>STAR LINE</h2>
-    <p>Zuari C&F</p></a>
     </li>
     <li><a href="#">
         <img src="../_assets/img/album-bb.jpg">
     <h2>CEMENT LINE</h2>
-    <p>Multi brand dealership</p></a>
     </li>
 </ul>
 </body>
